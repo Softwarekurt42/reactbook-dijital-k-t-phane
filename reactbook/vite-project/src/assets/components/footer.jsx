f4e78530a6285f2ce17749232315ac1b9f025943
Raşit Eken <144935222+Softwarekurt42@users.noreply.github.com>
2024-02-23 23:26:10 +0300

    dijital kütüphane

---
+++ reactbook/vite-project/src/assets/components/footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <footer>
+        <p className="text-center my-3">Tüm hakları saklıdır. 2024 YAKIŞIKLI YAZILIM</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
